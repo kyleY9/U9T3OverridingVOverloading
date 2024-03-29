33fd1975fb3740ff745b8cfc843cef0f72df0b6a
Kyle Yang <KyleY9@nycstudents.net>
2024-03-28 22:15:34 -0400

    Finished the Overriding vs. Overloading lab.

--- src/Dancer.java
+++ src/Dancer.java
@@ -9,4 +9,35 @@ public class Dancer extends Performer {
     public void pirouette(int spins) {
         System.out.println("I am spinning " + spins + " times!");
     }
+
+    @Override
+    public String getName() {
+        return super.getName() + " is a dancer!";
+    }
+
+    @Override
+    public void rehearse() {
+        pirouette(3);
+        super.rehearse();
+    }
+
+    @Override
+    public void callAgent() {
+        super.callAgent();
+        System.out.println("My shoes don't fit! Please pick me up a new pair.");
+    }
+
+    public void callAgent(int correctSize) {
+        callAgent();
+        System.out.println("I wear " + correctSize + " by the way!");
+    }
+
+    public void perform(boolean warmUp) {
+        if (warmUp) {
+            rehearse();
+            perform();
+        } else {
+            perform();
+        }
+    }
 }
